a2b96d551c3cb5c187458a38ccb1ffa277f9a916
Antoine Gauthier <antoine@MacBook-Pro-de-Antoine.local>
2019-12-02 19:14:56 +0100

    First draft of training script

---
+++ training/train.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
+input_train = pd.read_csv('../data/input_train.csv')
+X = input_train['question'].values
+
+output_train = pd.read_csv('../data/output_train.csv')
+y = output_train['intention'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# TODO : change default parameters of SVC
+clf = SVC()
+clf.fit(X_train, y_train)
